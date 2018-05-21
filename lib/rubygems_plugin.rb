650fb89e152d2fd9f41abba58a7efd593489c911
Marcin Kusiak <marcin.kusiak@stermedia.eu>
2018-05-21 17:15:29 +0200

    Test gem post install hooks

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,10 +1,11 @@
 Gem.post_install do
-  if File.exist?('yatta.txt')
-    p 1
-    f= File.open('yatta.txt', 'r')
-    p f.path
-  else
-    p 2
-    File.open('yatta.txt', 'w') { |f| f.write 'SUCC'}
-  end
+  Dir.pwd
+  # if File.exist?('yatta.txt')
+  #   p 1
+  #   f= File.open('yatta.txt', 'r')
+  #   p f.path
+  # else
+  #   p 2
+  #   File.open('yatta.txt', 'w') { |f| f.write 'SUCC'}
+  # end
 end
