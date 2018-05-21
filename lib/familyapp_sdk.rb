dad3c76d4a2862fd2cb941bc13082c386149bbca
Marcin Kusiak <marcin.kusiak@stermedia.eu>
2018-05-21 19:59:32 +0200

    Test gem post install hooks

--- lib/familyapp_sdk.rb
+++ lib/familyapp_sdk.rb
@@ -8,15 +8,36 @@ module FamilyappSdk
     def configure
       @config ||= Configuration.new
       yield(config)
+      load_rsa_key
     end
 
     def reset
       @config = Configuration.new
     end
+    
+    def load_rsa_key
+      if File.exist?(@config.rsa_key_path)
+        if @config.rsa_key_password.nil?
+          key = OpenSSL::PKey::RSA.new File.read(@config.rsa_key_path)
+        else
+          key = OpenSSL::PKey::RSA.new File.read(@config.rsa_key_path), @config.rsa_key_password
+        end
+      else
+        key = OpenSSL::PKey::RSA.new(2048)
+        if @config.rsa_key_password.nil?
+          file = File.open(@config.rsa_key_path, 'w') {|file| file.write key.to_pem}
+        else
+          cipher = OpenSSL::Cipher::AES256.new(:CBC)
+          password = Digest::SHA256.hexdigest SecureRandom.uuid
+          file = File.open(@config.rsa_key_path, 'w') {|file| file.write key.to_pem(cipher,password)}
+          @config.rsa_key_password = password
+        end
+        file.close
+      end
+      @rsa_key = key
+    end
   end
 end
-
-require 'familyapp_sdk/rsa_key_loader'
 
 require 'familyapp_sdk/engine'
 require 'familyapp_sdk/client'
--- lib/familyapp_sdk/configuration.rb
+++ lib/familyapp_sdk/configuration.rb
@@ -3,7 +3,7 @@ module FamilyappSdk
     DEFAULT_RSA_KEY_PATH = 'rsa.pem'
     attr_accessor :access_token, :verify_token, :api_host, :rsa_key_password, :rsa_key_path
 
-    def initialize(access_token: nil, verify_token: nil, api_host: nil , rsa_key_password: nil)
+    def initialize(access_token: nil, verify_token: nil, api_host: nil, rsa_key_password: nil)
       @access_token = access_token
       @verify_token = verify_token
       @api_host = api_host || 'https://api.familyapp.com'
